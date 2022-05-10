ec5f904425b594465f2a039178285221b9f191b8
enzomolloy <enzomolloy@gmail.com>
2022-05-10 15:28:54 -0300

    Texto

---
+++ Assets/Dataimputmanager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Dataimputmanager : MonoBehaviour
+{
+
+    public Text txtUserName;
+
+    public void ShowGreetings()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+}
